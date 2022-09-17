348b0743094fd3ab023bfa79d5352913787ea609
Trusty Cole <trustylatunde@gmail.com>
2022-09-17 07:51:27 +0000

    firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALe-ngAbEo4qZlG4tVHs0RACBsVFRi5fo",
+  authDomain: "farm-2427d.firebaseapp.com",
+  projectId: "farm-2427d",
+  storageBucket: "farm-2427d.appspot.com",
+  messagingSenderId: "919603634379",
+  appId: "1:919603634379:web:3ac342398bc2ab533a7b4e",
+  measurementId: "G-J86WC5R3L6"
+};
+
+export default firebaseConfig
